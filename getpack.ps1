73ec37c929aed655fd26b30a09dd2b5181456a8c
fere <anton@anton-z.ru>
2021-07-19 12:41:18 +0800

    Create getpack.ps1

---
+++ getpack.ps1
@@ -0,0 +1,9 @@
+$pack="SA100D23"
+Copy-Item -Path "\\cab-vsp-dp00015\smspkgc$\$pack\*" -Destination "C:\Temp\SIGMA\$pack\" -Force
+
+$pack="IM00004B"
+Copy-Item -Path "\\cab-vsp-dp00015\smspkgc$\$pack\*" -Destination "C:\Temp\SIGMA\$pack\" -Force
+
+$pack="SA10085C"
+Copy-Item -Path "\\cab-vsp-dp00015\smspkgc$\$pack\*" -Destination "C:\Temp\SIGMA\$pack\" -Force
+Explorer C:\Temp\SIGMA\
