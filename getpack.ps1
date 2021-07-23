0c23f85362eb85922da753e4ff523c096a2280d0
fere <anton@anton-z.ru>
2021-07-24 00:38:40 +0800

    Update getpack.ps1

--- getpack.ps1
+++ getpack.ps1
@@ -6,4 +6,14 @@ Copy-Item -Path "\\cab-vsp-dp00015\smspkgc$\$pack\*" -Destination "C:\Temp\SIGMA
 
 $pack="SA10085C"
 Copy-Item -Path "\\cab-vsp-dp00015\smspkgc$\$pack\*" -Destination "C:\Temp\SIGMA\$pack\" -Force
+
+$pack="SA100995"
+Copy-Item -Path "\\cab-vsp-dp00015\smspkgc$\$pack\*" -Destination "C:\Temp\SIGMA\$pack\" -Force
+
 Explorer C:\Temp\SIGMA\
+
+$pack="SA100995"
+cd C:\Temp\SIGMA\$pack\Citrix_Workspace_Install_Only.vbs
+
+
+
